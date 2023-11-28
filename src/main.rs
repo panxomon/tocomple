e86f33d79946c6c0c8a81f21804167ba6736daaf
mbadilla-linets <Pax.linets31522>
2023-11-27 21:07:57 -0300

    change to egui framework

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,119 @@
-use iced::{Element, Sandbox, Settings, Color, Background, Command, Length};
-use iced::widget::{text_input, container, scrollable, Button, Text, Column, button};
-use iced::widget::TextInput;
-use iced::window;
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-pub fn main() -> iced::Result {
-    Tocomple::run(Settings {
-        window: iced::window::Settings {
-            size: (400, 650),
-            ..iced::window::Settings::default()
-        },
-        ..Settings::default()
-    })
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 240.0]) // wide enough for the drag-drop overlay text
+            .with_drag_and_drop(true),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Native file dialogs and drag-and-drop files",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
 }
 
 #[derive(Default)]
-struct Tocomple {
-    input_text1: String,
-    input_text2: String,
-    button_state1: button::State,
+struct MyApp {
+    dropped_files: Vec<egui::DroppedFile>,
+    picked_path: Option<String>,
 }
 
-#[derive(Debug, Clone)]
-enum Message {
-    TextInputChanged1(String),
-    TextInputChanged2(String),
-    ButtonClicked,
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Drag-and-drop files onto the window!");
+
+            if ui.button("Open file…").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.picked_path = Some(path.display().to_string());
+                }
+            }
+
+            if let Some(picked_path) = &self.picked_path {
+                ui.horizontal(|ui| {
+                    ui.label("Picked file:");
+                    ui.monospace(picked_path);
+                });
+            }
+
+            // Show dropped files (if any):
+            if !self.dropped_files.is_empty() {
+                ui.group(|ui| {
+                    ui.label("Dropped files:");
+
+                    for file in &self.dropped_files {
+                        let mut info = if let Some(path) = &file.path {
+                            path.display().to_string()
+                        } else if !file.name.is_empty() {
+                            file.name.clone()
+                        } else {
+                            "???".to_owned()
+                        };
+
+                        let mut additional_info = vec![];
+                        if !file.mime.is_empty() {
+                            additional_info.push(format!("type: {}", file.mime));
+                        }
+                        if let Some(bytes) = &file.bytes {
+                            additional_info.push(format!("{} bytes", bytes.len()));
+                        }
+                        if !additional_info.is_empty() {
+                            info += &format!(" ({})", additional_info.join(", "));
+                        }
+
+                        ui.label(info);
+                    }
+                });
+            }
+        });
+
+        preview_files_being_dropped(ctx);
+
+        // Collect dropped files:
+        ctx.input(|i| {
+            if !i.raw.dropped_files.is_empty() {
+                self.dropped_files = i.raw.dropped_files.clone();
+            }
+        });
+    }
 }
 
-impl Sandbox for Tocomple {
-    type Message = Message;
+/// Preview hovering files:
+fn preview_files_being_dropped(ctx: &egui::Context) {
+    use egui::*;
+    use std::fmt::Write as _;
 
-    fn new() -> Tocomple {
-        Tocomple::default()
-    }
-
-    fn title(&self) -> String {
-        String::from("kafka client")
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::TextInputChanged1(text) => {
-                self.input_text1 = text;
+    if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
+        let text = ctx.input(|i| {
+            let mut text = "Dropping files:\n".to_owned();
+            for file in &i.raw.hovered_files {
+                if let Some(path) = &file.path {
+                    write!(text, "\n{}", path.display()).ok();
+                } else if !file.mime.is_empty() {
+                    write!(text, "\n{}", file.mime).ok();
+                } else {
+                    text += "\n???";
+                }
             }
-            Message::TextInputChanged2(text) => {
-                self.input_text2 = text;
-            }
-            Message::ButtonClicked => {
-                println!("Input 1: {}", self.input_text1);
-                println!("Input 2: {}", self.input_text2);
-            }
-        }
+            text
+        });
+
+        let painter =
+            ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
+
+        let screen_rect = ctx.screen_rect();
+        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
+        painter.text(
+            screen_rect.center(),
+            Align2::CENTER_CENTER,
+            text,
+            TextStyle::Heading.resolve(&ctx.style()),
+            Color32::WHITE,
+        );
     }
-
-    fn view(&self) -> Element<Message> {
-        let textbox1 = text_input("", &self.input_text1)
-            .on_input(Message::TextInputChanged1);
-
-        let textbox2 = text_input("", &self.input_text2)
-            .on_input(Message::TextInputChanged2);;
-
-        let button =Button::new("DENTRAR").on_press(Message::ButtonClicked);
-
-        let content = Column::new()
-            .spacing(20)
-            .padding(40)
-            .push(textbox1)
-            .push(textbox2)
-            .push(button);
-
-        scrollable(
-            container(content)
-                .width(Length::Fill)
-                .padding(40)
-                .center_x(),
-        )
-            .into()
-    }
-
 }
